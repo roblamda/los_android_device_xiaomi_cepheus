21a43d37b7ea44839626e6b9956f48f6b7a759b4
roblamda <72200197+roblamda@users.noreply.github.com>
2025-06-13 19:45:55 +0200

    cepheus: RRO: Configure aux camera + framerates for Aperture

--- device.mk
+++ device.mk
@@ -30,6 +30,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    ApertureOverlayDevice \
     FrameworkResOverlayDevice \
     LineageSystemUIOverlayDevice \
     SettingsOverlayDevice \
